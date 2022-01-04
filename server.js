4e4d09af93a7a2e13705edd93ab40aac570647c3
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-01-04 11:59:35 -0700

    Added port 3000, prettier 🪄

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const express = require('express'),
   app = express(),
   // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
   // so that your API is remotely testable by FCC
-  cors = require('cors');
+  cors = require('cors'),
+  port = process.env.PORT || 3000;
 
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
@@ -45,6 +46,6 @@ app.get('/api/whoami', (req, res) => {
   });
 });
 
-const listener = app.listen(process.env.PORT, () =>
+const listener = app.listen(port, () =>
   log('Your app is listening on port ' + listener.address().port)
 );
